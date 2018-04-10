43320e10e50c534d48c3c72c1ef2076aefb00374
davizzard <davizzard@gmail.com>
2018-04-10 08:59:24 -0400

    Amazon EC2 Test and Comparison (2,3Gb File)

---
+++ src/scripts/copy_files.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp ./files_to_copy/createAcc_test.g ./../../../../alruiz12/goObjStore/src/httpGo/createAcc_test.go
+cp ./files_to_copy/Proxy.g ./../../../../alruiz12/goObjStore/src/httpGo/Proxy.go
